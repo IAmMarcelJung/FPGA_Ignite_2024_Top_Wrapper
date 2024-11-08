2ff79b61a453193d8b79a48ce630130ef9a04862
IAmMarcelJung <MarcelJung97@web.de>
2024-11-08 21:09:13 +0100

    furhter adaptions and comments, still many TODOs

--- top_wrapper_test.v
+++ top_wrapper_test.v
@@ -1,27 +1,31 @@
 
-module top_wrapper_test (
+module top_wrapper_test #(
+    parameter NUM_OF_TOTAL_FABRIC_IOS = 32,  //TODO: TBD
+    parameter NUM_OF_LOGIC_ANALYZER_BITS = 128,
+    parameter WB_DATA_WIDTH = 32
+) (
     // Wishbone Slave ports (WB MI A)
     input wb_clk_i,
     input wbs_stb_i,
     input wbs_cyc_i,
     input wbs_we_i,
-    input [31:0] wbs_dat_i,
-    input [31:0] wbs_adr_i,
-    output [31:0] wbs_dat_o,
+    input [WB_DATA_WIDTH-1:0] wbs_dat_i,
+    input [WB_DATA_WIDTH-1:0] wbs_adr_i,
+    output [WB_DATA_WIDTH-1:0] wbs_dat_o,
     output reg wbs_ack_o,
 
     // Logic Analyzer Signals
-    input  [127:0] la_data_in,
-    output [127:0] la_data_out,
-    input  [127:0] la_oenb,
+    input  [NUM_OF_LOGIC_ANALYZER_BITS-1:0] la_data_in,
+    output [NUM_OF_LOGIC_ANALYZER_BITS-1:0] la_data_out,
+    input  [NUM_OF_LOGIC_ANALYZER_BITS-1:0] la_oenb,
 
     // IOs
-    input  [31-1:0] io_in,
-    output [31-1:0] io_out,
-    output [31-1:0] io_oeb,
+    input  [NUM_OF_TOTAL_FABRIC_IOS-1:0] io_in,
+    output [NUM_OF_TOTAL_FABRIC_IOS-1:0] io_out,
+    output [NUM_OF_TOTAL_FABRIC_IOS-1:0] io_oeb,
 
     // Independent clock (on independent integer divider)
-    // TODO: check if to use this or wb_clk
+    // NOTE: unused, we are using the wishbone clock
     input user_clock2
 );
 
@@ -34,24 +38,27 @@ module top_wrapper_test (
     localparam FrameSelectWidth = 5;
     localparam RowSelectWidth = 5;
 
+    // The number of IOs that can be used the FPGA user design
     localparam NUM_FABRIC_USER_IOS = 16;
     localparam [31:0] BASE_WB_ADDRESS = 32'h3000_0000;
     localparam [31:0] CONFIG_DATA_WB_ADDRESS = BASE_WB_ADDRESS;
     localparam [31:0] TO_FABRIC_IOS_WB_ADDRESS = BASE_WB_ADDRESS + 4;
 
-    // External USER ports
-    //inout [16-1:0] PAD; // these are for Dirk and go to the pad ring
     wire [NUM_FABRIC_USER_IOS-1:0] I_top;
     wire [NUM_FABRIC_USER_IOS-1:0] T_top;
     wire [NUM_FABRIC_USER_IOS-1:0] O_top;
-    wire [48-1:0] A_config_C;
-    wire [48-1:0] B_config_C;
 
-    wire CLK;  // This clock can go to the CPU (connects to the fabric LUT output flops
+    wire CLK;  // This clock can go to the CPU (connects to the fabric LUT output flops)
+    wire resetn;
 
     // CPU configuration port
     wire SelfWriteStrobe;  // must decode address and write enable
     wire [32-1:0] SelfWriteData;  // configuration data write port
+
+    // Wishbone configuration signals
+    wire config_strobe;
+    wire fabric_strobe;
+    reg [31:0] config_data;
 
     // UART configuration port
     wire Rx;
@@ -62,34 +69,8 @@ module top_wrapper_test (
     wire s_clk;
     wire s_data;
 
-    //BlockRAM ports
-    wire [192-1:0] RAM2FAB_D;
-    wire [192-1:0] FAB2RAM_D;
-    wire [96-1:0] FAB2RAM_A;
-    wire [48-1:0] FAB2RAM_C;
-    wire [48-1:0] Config_accessC;
 
-    // Signal declarations
-    wire [(NumberOfRows*FrameBitsPerRow)-1:0] FrameRegister;
-
-    wire [(MaxFramesPerCol*NumberOfCols)-1:0] FrameSelect;
-
-    wire [(FrameBitsPerRow*(NumberOfRows+2))-1:0] FrameData;
-
-    wire [FrameBitsPerRow-1:0] FrameAddressRegister;
-    wire LongFrameStrobe;
-    wire [31:0] LocalWriteData;
-    wire LocalWriteStrobe;
-    wire [RowSelectWidth-1:0] RowSelect;
-
-    wire external_clock;
-    wire [1:0] clk_sel;
-
-    wire config_strobe;
-    wire fabric_strobe;
-    reg [31:0] config_data;
-
-    //latch for config_strobe
+    // Latch for config_strobe
     reg latch_config_strobe = 0;
     reg config_strobe_reg1 = 0;
     reg config_strobe_reg2 = 0;
@@ -98,7 +79,28 @@ module top_wrapper_test (
     wire latch_config_strobe_inverted2;
 
     // TODO: think about if the parameters have to be set
-    eFPGA_top eFPGA_top_i ();
+    // TODO: TRIPLE (!!!) check everything here!!!
+    eFPGA_top eFPGA_top_i (
+        .A_config_C(),  // NOTE: Dirk said to leave this empty since its not needed
+        .B_config_C(),  // NOTE: Dirk said to leave this empty since its not needed
+        .Config_accessC(),  // NOTE: Dirk said to leave this empty since its not needed
+        .I_top(I_top),
+        .O_top(O_top),
+        .T_top(T_top),
+        .UIO_BOT_UIN(),  //TODO: TBD
+        .UIO_BOT_UOUT(),  //TODO: TBD
+        .UIO_TOP_UIN(),  //TODO: TBD
+        .UIO_TOP_UOUT(),  //TODO TBD
+        .CLK(CLK),
+        .resten(resetn),  //TODO: resetn still has to be connected to a pin, probably one of io0-2?
+        .SelfWriteStrobe(SelfWriteStrobe),
+        .SelfWriteData(SelfWriteData),
+        .Rx(Rx),
+        .ComActive(ComActive),
+        .ReceiveLED(ReceiveLED),
+        .s_clk(s_clk),
+        .s_data(s_data)
+    );
 
 
     always @(*) begin
@@ -110,11 +112,16 @@ module top_wrapper_test (
             latch_config_strobe = 1;
         end
     end
-    //assign latch_config_strobe_inverted1 = (!latch_config_strobe);            //This are the two inverters
+
+    //These are the two inverters
+    //NOTE: keep the comment for reference
+    //assign latch_config_strobe_inverted1 = (!latch_config_strobe);
     sky130_fd_sc_hd__inv latch_config_strobe_inv_0 (
         .Y(latch_config_strobe_inverted1),
         .A(latch_config_strobe)
     );
+
+    //NOTE: keep the comment for reference
     //assign latch_config_strobe_inverted2 = (!latch_config_strobe_inverted1);
     sky130_fd_sc_hd__inv latch_config_strobe_inv_1 (
         .Y(latch_config_strobe_inverted2),
@@ -126,43 +133,6 @@ module top_wrapper_test (
         config_strobe_reg3 <= config_strobe_reg2;
     end
     assign config_strobe = (config_strobe_reg3 && (!config_strobe_reg2)); //posedge pulse for config strobe
-
-
-    //latch for fabric_strobe
-    reg  latch_fabric_strobe = 0;
-    reg  fabric_strobe_reg1 = 0;
-    reg  fabric_strobe_reg2 = 0;
-    reg  fabric_strobe_reg3 = 0;
-    wire latch_fabric_strobe_inverted1;
-    wire latch_fabric_strobe_inverted2;
-
-    always @(*) begin
-        if (fabric_strobe_reg2) begin
-            latch_fabric_strobe = 0;
-        end else if (latch_fabric_strobe_inverted2) begin
-            latch_fabric_strobe = 0;
-        end else if(wbs_stb_i && wbs_cyc_i && wbs_we_i && !wbs_sta_o && (wbs_adr_i == TO_FABRIC_IOS_WB_ADDRESS)) begin
-            latch_fabric_strobe = 1;
-        end
-    end
-    //assign latch_fabric_strobe_inverted1 = (!latch_fabric_strobe);            //This are the two inverters
-    sky130_fd_sc_hd__inv latch_fabric_strobe_inv_0 (
-        .Y(latch_fabric_strobe_inverted1),
-        .A(latch_fabric_strobe)
-    );
-    //assign latch_fabric_strobe_inverted2 = (!latch_fabric_strobe_inverted1);
-    sky130_fd_sc_hd__inv latch_fabric_strobe_inv_1 (
-        .Y(latch_fabric_strobe_inverted2),
-        .A(latch_fabric_strobe_inverted1)
-    );
-    always @(posedge CLK) begin
-        fabric_strobe_reg1 <= latch_fabric_strobe;
-        fabric_strobe_reg2 <= fabric_strobe_reg1;
-        fabric_strobe_reg3 <= fabric_strobe_reg2;
-    end
-
-    //posedge pulse for config strobe
-    assign fabric_strobe = (fabric_strobe_reg3 && (!fabric_strobe_reg2));
 
     //config data register
     always @(posedge wb_clk_i) begin
@@ -179,17 +149,22 @@ module top_wrapper_test (
             wbs_ack_o <= (wbs_stb_i && !wbs_sta_o && (wbs_adr_i == CONFIG_DATA_WB_ADDRESS));
     end
 
+    // TODO: probably use io0 -> io1 for different stuff
+
+    // TODO: think about if this is the best pin, but it should be fine
+    // An alternative could be to use io_in[0] since 1 and 2 are quite close
+    // together on the board
+    assign resetn = io_in[2];
 
 
-    //TODO: probably use io0 -> io2 for different stuff
-
+    // NOTE: this was just taken from the previous shuttles
     assign s_clk = io_in[3];
     assign s_data = io_in[4];
     assign Rx = io_in[5];
     assign io_out[6] = ReceiveLED;
 
     //TODO: double check, but should be fine
-    assign io_oeb[6:3] = 4'b0111;
+    assign io_oeb[6:2] = 5'b01111;
 
     assign SelfWriteStrobe = config_strobe;
     assign SelfWriteData = config_data;
@@ -202,7 +177,8 @@ module top_wrapper_test (
     };
 
     // TODO: set correct size
-    assign O_top[23:18] = io_in[30:25];
+    // TODO: These have to be multiplexed with the user modules
+    assign O_top[23:0] = io_in[30:7];
     assign io_out[30:7] = I_top;
     assign io_oeb[30:7] = T_top;
 endmodule
