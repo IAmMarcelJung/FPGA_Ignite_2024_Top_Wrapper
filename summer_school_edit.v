a1273e166094de045088dd5afc2c4748c839a1da
IAmMarcelJung <MarcelJung97@web.de>
2024-11-10 00:20:49 +0100

    fix: totally change the way how the IOs are assigned, used parameters for assignment, also rearranged the IOs

--- summer_school_edit.v
+++ summer_school_edit.v
@@ -30,13 +30,38 @@ module top_wrapper_test #(
     input user_clock2
 );
 
- 
+
 
     // The number of IOs that can be used the FPGA user design
     localparam NUM_FABRIC_USER_IOS = 16;
     localparam [31:0] BASE_WB_ADDRESS = 32'h3000_0000;
     localparam [31:0] CONFIG_DATA_WB_ADDRESS = BASE_WB_ADDRESS;
     localparam [31:0] TO_FABRIC_IOS_WB_ADDRESS = BASE_WB_ADDRESS + 4;
+
+    // Fabric IOs
+    localparam RESETN_IO = 0;
+    localparam SELECT_MODULE_IO = 1;
+    localparam SEL_IO = 2;
+    localparam S_CLK_IO = 3;
+    localparam S_DATA_IO = 4;
+    localparam EFPGA_UART_RX_IO = 5;
+    localparam RECEIVE_LED_IO = 6;
+
+    // eFPGA IOs
+    localparam EFPGA_USED_NUM_IOS = 13;  // Due to pin count limitation, we don't use all eFPGA IOs
+    localparam EFPGA_IO_LOWEST = 7;
+    localparam EFPGA_IO_HIGHEST = EFPGA_IO_LOWEST + EFPGA_USED_NUM_IOS - 1;
+
+    // VGA IOs
+    localparam VGA_NUM_IOS = 8;
+    localparam VGA_IO_LOWEST = EFPGA_IO_HIGHEST + 1;  // VGA is located after the eFPGA pins
+    localparam VGA_IO_HIGHEST = VGA_IO_LOWEST + VGA_NUM_IOS - 1;
+
+    // NOVACORE IOs
+    localparam NOVACORE_UART_RX_IO = VGA_IO_HIGHEST + 1; // NOVACORE UART is located after the VGA pins
+    localparam NOVACORE_UART_TX_IO = NOVACORE_UART_RX_IO + 1;
+
+
 
     wire [NUM_FABRIC_USER_IOS-1:0] I_top;
     wire [NUM_FABRIC_USER_IOS-1:0] T_top;
@@ -55,7 +80,7 @@ module top_wrapper_test #(
     reg [31:0] config_data;
 
     // UART configuration port
-    wire Rx;
+    wire efpga_uart_rx;
     wire ComActive;
     wire ReceiveLED;
 
@@ -73,17 +98,15 @@ module top_wrapper_test #(
     wire latch_config_strobe_inverted2;
 
 
-        wire [17:0] UI0_TOP_UOUT_PAD;
-        reg [39:0] UI0_TOP_UIN_PAD;
-        wire [99:0] UI0_BOT_UOUT_PAD;
-        reg [67:0] UI0_BOT_UIN_PAD;
+    wire [17:0] UI0_TOP_UOUT_PAD;
+    reg [39:0] UI0_TOP_UIN_PAD;
+    wire [99:0] UI0_BOT_UOUT_PAD;
+    reg [67:0] UI0_BOT_UIN_PAD;
 
 
-
-
-     // TODO: think about if the parameters have to be set
+    // TODO: think about if the parameters have to be set
     // TODO: TRIPLE (!!!) check everything here!!!
-   flexbex_soc_top flexbex_eFPGA (
+    flexbex_soc_top flexbex_eFPGA (
         .A_config_C(),  // NOTE: Dirk said to leave this empty since its not needed
         .B_config_C(),  // NOTE: Dirk said to leave this empty since its not needed
         .Config_accessC(),  // NOTE: Dirk said to leave this empty since its not needed
@@ -92,8 +115,8 @@ module top_wrapper_test #(
         .resten(resetn),  //TODO: resetn still has to be connected to a pin, probably one of io0-2?
         .SelfWriteStrobe(SelfWriteStrobe),
         .SelfWriteData(SelfWriteData),
-        .Rx(Rx),
-        .ComActive(), //Dirk said to not connect it
+        .Rx(efpga_uart_rx),
+        .ComActive(),  //Dirk said to not connect it
         .ReceiveLED(ReceiveLED),
         .s_clk(s_clk),
         .s_data(s_data),
@@ -103,28 +126,28 @@ module top_wrapper_test #(
         .T_top(T_top),
 
         .UI0_TOP_UOUT_PAD(UI0_TOP_UOUT_PAD),
-        .UI0_TOP_UIN_PAD(UI0_TOP_UIN_PAD),
+        .UI0_TOP_UIN_PAD (UI0_TOP_UIN_PAD),
         .UI0_BOT_UOUT_PAD(UI0_BOT_UOUT_PAD),
-        .UI0_BOT_UIN_PAD(UI0_BOT_UIN_PAD)
+        .UI0_BOT_UIN_PAD (UI0_BOT_UIN_PAD)
 
-     );
+    );
 
     //posit co-processor (eFPGA and LA)
-      // Define intermediate signals
-    reg issue_valid;                            // Signal for issue validity
-    reg [31:0] issue_req_instr;                 // Signal for issue request instruction (assuming 32-bit width)
-    reg register_valid;                         // Signal for register validity
-    reg [31:0] register_rs[1:0];                      // Signal for register sources (2 bits)
-    reg [1:0]register_rs_valid;                      // Signal for register sources validity
-    reg result_ready;                           // Signal for result readiness
+    // Define intermediate signals
+    reg issue_valid;  // Signal for issue validity
+    reg [31:0] issue_req_instr;  // Signal for issue request instruction (assuming 32-bit width)
+    reg register_valid;  // Signal for register validity
+    reg [31:0] register_rs[1:0];  // Signal for register sources (2 bits)
+    reg [1:0] register_rs_valid;  // Signal for register sources validity
+    reg result_ready;  // Signal for result readiness
 
-    wire issue_ready;                           // Signal for issue readiness
-    wire issue_resp_accept;                     // Signal for issue response accept
-    wire issue_resp_writeback;                  // Signal for issue response writeback
-    wire [1:0] issue_resp_register_read;        // Signal for issue response register read (2 bits)
-    wire register_ready;                        // Signal for register readiness
-    wire result_valid;                          // Signal for result validity
-    wire [31:0] result_data;                    // Signal for result data (assuming 32-bit width)
+    wire issue_ready;  // Signal for issue readiness
+    wire issue_resp_accept;  // Signal for issue response accept
+    wire issue_resp_writeback;  // Signal for issue response writeback
+    wire [1:0] issue_resp_register_read;  // Signal for issue response register read (2 bits)
+    wire register_ready;  // Signal for register readiness
+    wire result_valid;  // Signal for result validity
+    wire [31:0] result_data;  // Signal for result data (assuming 32-bit width)
 
     pau pau_inst (
         .clk(wb_clk_i),
@@ -148,7 +171,7 @@ module top_wrapper_test #(
     // THE RING
     reg en;
     wire [7:0] d_out;
-    ro_top  ring_inst (
+    ro_top ring_inst (
         .clk(wb_clk_i),
         .en(en),
         .d_out(d_out)
@@ -156,22 +179,22 @@ module top_wrapper_test #(
 
 
     // VGA_Ignite
-   reg sync;
-   reg [2:0] mode;
-   reg [7:0] data_i;
-   reg stb_i;
-   reg ack_o;
+    reg sync;
+    reg [2:0] mode;
+    reg [7:0] data_i;
+    reg stb_i;
+    reg ack_o;
 
-   wire [7:0]data_o;
-   wire ack_i;
-   wire [1:0]vga_r;
-   wire [1:0]vga_g;
-   wire [1:0]vga_b;
+    wire [7:0] data_o;
+    wire ack_i;
+    wire [1:0] vga_r;
+    wire [1:0] vga_g;
+    wire [1:0] vga_b;
 
-   wire hsync;
-   wire vsync;
+    wire hsync;
+    wire vsync;
 
-   ppu ppu_inst (
+    ppu ppu_inst (
         .clk(wb_clk_i),
         .rst(!resetn),
         .sync(UI0_BOT_UOUT_PAD[0]),
@@ -185,95 +208,109 @@ module top_wrapper_test #(
     );
 
     // Instantiate VGA Driver module
-    assign io_oeb[17:10] = 8'b00000000;
-    assign io_out[17:10] = {vga_r,vga_g,vga_b,hsync,vsync};
+    assign io_oeb[VGA_IO_HIGHEST:VGA_IO_LOWEST] = 8'b00000000;
+    assign io_out[VGA_IO_HIGHEST:VGA_IO_LOWEST] = {vga_r, vga_g, vga_b, hsync, vsync};
 
     vga_driver vga_driver_inst (
         .clk_pix(wb_clk_i),
         .rst_pix(!resetn),
-        .wb_data(data_o),      // PPU's data output is written to VGA
-        .vga_r(vga_r),
-        .vga_g(vga_g),
-        .vga_b(vga_b),
-        .sx(), //simulation signals
-        .sy(), //simulation signals
-        .hsync(hsync),
-        .vsync(vsync),
-        .de()   //simulation signals
+        .wb_data(data_o),    // PPU's data output is written to VGA
+        .vga_r  (vga_r),
+        .vga_g  (vga_g),
+        .vga_b  (vga_b),
+        .sx     (),          //simulation signals
+        .sy     (),          //simulation signals
+        .hsync  (hsync),
+        .vsync  (vsync),
+        .de     ()           //simulation signals
     );
 
     //NOVACORE
 
-    assign io_oeb[8]= 1'b1;
-    assign io_oeb[9]=1'b0;
-    toplevel nova_core (.system_clk(wb_clk_i),.system_clk_locked(),.reset_n(resetn),.uart0_txd(io_out[9]), .uart_rxd(io_in[8]));
+    assign io_oeb[NOVACORE_UART_TX_IO] = 1'b0;
+    assign io_oeb[NOVACORE_UART_RX_IO] = 1'b1;
+    toplevel nova_core (
+        .system_clk(wb_clk_i),
+        .system_clk_locked(),
+        .reset_n(resetn),
+        .uart0_txd(io_out[NOVACORE_UART_TX_IO]),
+        .uart_rxd(io_in[NOVACORE_UART_RX_IO])
+    );
 
 
 
-    wire select_module,sel;
-
-     // TODO this needs to be checked thoroughly
-
-     assign io_oeb[1]=1'b1;
-     assign io_oeb[7]=1'b1;
-     assign select_module = io_in[1];
-     assign sel= io_in[7];
+    wire select_module, sel;
 
 
-     // Module Select
-     always @(*) begin
-     case (select_module)
+
+    // Module Select
+    always @(*) begin
+        case (select_module)
 
 
-     // THE RING
-     1'b0:  begin
-            case(sel)
-            
-            // Logic Analyzer
-            1: begin
-               if(la_oenb[7:0])
-               la_data_in [7:0]<= d_out;
-               else
-               en <= la_data_out[8];
-               end
-               
-              default: //eFPGA
+            // THE RING
+            1'b0: begin
+                case (sel)
+
+                    // Logic Analyzer
+                    1: begin
+                        if (la_oenb[7:0]) la_data_in[7:0] <= d_out;
+                        else en <= la_data_out[8];
+                    end
+
+                    default: //eFPGA
                  begin
-               //inputs
-               en <= UIO_BOT_UOUT_PAD[13];
-               //outputs
-               UIO_BOT_UIN_PAD[8:1] <= d_out;
-               end
-               endcase
+                        //inputs
+                        en <= UIO_BOT_UOUT_PAD[13];
+                        //outputs
+                        UIO_BOT_UIN_PAD[8:1] <= d_out;
+                    end
+                endcase
             end
 
 
-     default: //posit coprocessor
+            default: //posit coprocessor
                 begin
-            case (sel)
+                case (sel)
 
-            // LA
-            1: begin
-               //inputs
-               if (la_oenb[101:1])
-                    {issue_valid,issue_req_instr,register_valid,register_rs[1],register_rs[0],register_rs_valid,result_ready} <= la_data_in[101:1]; // !!! Double check this
-               else
-                     la_data_out [39:1] <= {issue_ready,issue_resp_accept, issue_resp_writeback,issue_resp_register_read,register_ready,result_valid, result_data};
-               end
+                    // LA
+                    1: begin
+                        //inputs
+                        if (la_oenb[101:1])
+                            {issue_valid,issue_req_instr,register_valid,register_rs[1],register_rs[0],register_rs_valid,result_ready} <= la_data_in[101:1]; // !!! Double check this
+                        else
+                            la_data_out[39:1] <= {
+                                issue_ready,
+                                issue_resp_accept,
+                                issue_resp_writeback,
+                                issue_resp_register_read,
+                                register_ready,
+                                result_valid,
+                                result_data
+                            };
+                    end
 
-               default: // eFPGA
+                    default: // eFPGA
             begin
-               // inputs
-               {issue_req_instr[31:18],issue_valid,issue_req_instr,register_valid,register_rs[1],register_rs[0],register_rs_valid,result_ready} <= UIO_BOT_UOUT_PAD[95:13];
-                issue_req_instr [17:0] <= UIO_TOP_UOUT_PAD[17:0];
+                        // inputs
+                        {issue_req_instr[31:18],issue_valid,issue_req_instr,register_valid,register_rs[1],register_rs[0],register_rs_valid,result_ready} <= UIO_BOT_UOUT_PAD[95:13];
+                        issue_req_instr[17:0] <= UIO_TOP_UOUT_PAD[17:0];
 
-               //outputs
-               UIO_BOT_UIN_PAD[39:1] <= {issue_ready,issue_resp_accept, issue_resp_writeback,issue_resp_register_read,register_ready,result_valid, result_data} ;
-               end
-               endcase
-           end
-       endcase
-           end
+                        //outputs
+                        UIO_BOT_UIN_PAD[39:1] <= {
+                            issue_ready,
+                            issue_resp_accept,
+                            issue_resp_writeback,
+                            issue_resp_register_read,
+                            register_ready,
+                            result_valid,
+                            result_data
+                        };
+                    end
+                endcase
+            end
+        endcase
+    end
 
 
 
@@ -323,35 +360,34 @@ module top_wrapper_test #(
             wbs_ack_o <= (wbs_stb_i && !wbs_sta_o && (wbs_adr_i == CONFIG_DATA_WB_ADDRESS));
     end
 
-    // TODO: probably use io0 -> io1 for different stuff
 
-    // TODO: think about if this is the best pin, but it should be fine
-    // An alternative could be to use io_in[0] since 1 and 2 are quite close
-    // together on the board
-    assign resetn = io_in[2];
+    // TODO: this needs to be checked thoroughly
 
-
-    // NOTE: this was just taken from the previous shuttles
-    assign s_clk = io_in[3];
-    assign s_data = io_in[4];
-    assign Rx = io_in[5];
-    assign io_out[6] = ReceiveLED;
+    assign resetn = io_in[RESETN_IO];
+    assign select_module = io_in[SELECT_MODULE_IO];
+    assign sel = io_in[SEL_IO];
+    assign s_clk = io_in[S_CLK_IO];
+    assign s_data = io_in[S_DATA_IO];
+    assign efpga_uart_rx = io_in[EFPGA_UART_RX_IO];
+    assign io_out[RECEIVE_LED_IO] = ReceiveLED;
 
     //TODO: double check, but should be fine
-    assign io_oeb[6:2] = 5'b01111;
+    assign io_oeb[RESETN_IO] = 1'b1;
+    assign io_oeb[SELECT_MODULE_IO] = 1'b1;
+    assign io_oeb[SEL_IO] = 1'b1;
+    assign io_oeb[S_CLK_IO] = 1'b1;
+    assign io_oeb[S_DATA_IO] = 1'b1;
+    assign io_oeb[EFPGA_UART_RX_IO] = 1'b1;
+    assign io_oeb[RECEIVE_LED_IO] = 1'b0;  // The only output of the fabric IOs
 
     assign SelfWriteStrobe = config_strobe;
     assign SelfWriteData = config_data;
 
     assign CLK = wb_clk_i;
 
-    // TODO: rethink about these and connect more to the logic analyzers
-    assign la_data_out[103:102] = {ReceiveLED, Rx
-    };
+    assign la_data_out[103:102] = {ReceiveLED, efpga_uart_rx};
 
-    // TODO: set correct size
-    // TODO: These have to be multiplexed with the user modules
-    assign O_top[12:0] = io_in[30:18];
-    assign io_out[30:18] = I_top[12:0];
-    assign io_oeb[30:18] = T_top[12:0];
+    assign O_top[EFPGA_USED_NUM_IOS-1:0] = io_in[EFPGA_IO_HIGHEST:EFPGA_IO_LOWEST];
+    assign io_out[EFPGA_IO_HIGHEST:EFPGA_IO_LOWEST] = I_top[EFPGA_USED_NUM_IOS-1:0];
+    assign io_oeb[EFPGA_IO_HIGHEST:EFPGA_IO_LOWEST] = T_top[EFPGA_USED_NUM_IOS-1:0];
 endmodule
